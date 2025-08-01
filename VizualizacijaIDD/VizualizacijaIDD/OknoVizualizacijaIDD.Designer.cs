6ad2ac28221f2c4cbc2961471ef5b8d3161b1b23
tejablazic <tejablazic12@gmail.com>
2025-08-01 13:22:34 +0200

    Add project files.

---
+++ VizualizacijaIDD/VizualizacijaIDD/OknoVizualizacijaIDD.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace VizualizacijaIDD
+{
+    partial class OknoVizualizacijaIDD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUstvari = new System.Windows.Forms.Button();
+            this.tbxUstvari = new System.Windows.Forms.TextBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.tbxDodaj = new System.Windows.Forms.TextBox();
+            this.btnOdstrani = new System.Windows.Forms.Button();
+            this.tbxOdstrani = new System.Windows.Forms.TextBox();
+            this.btnIsci = new System.Windows.Forms.Button();
+            this.tbxIsci = new System.Windows.Forms.TextBox();
+            this.btnPonastavi = new System.Windows.Forms.Button();
+            this.pnlPrikaz = new System.Windows.Forms.Panel();
+            this.btnNaprej = new System.Windows.Forms.Button();
+            this.btnNazaj = new System.Windows.Forms.Button();
+            this.lblHitrost = new System.Windows.Forms.Label();
+            this.rdbPol = new System.Windows.Forms.RadioButton();
+            this.rdbEna = new System.Windows.Forms.RadioButton();
+            this.rdbRocno = new System.Windows.Forms.RadioButton();
+            this.rdbEnaInPol = new System.Windows.Forms.RadioButton();
+            this.lblRazlaga = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUstvari
+            // 
+            this.btnUstvari.Location = new System.Drawing.Point(136, 12);
+            this.btnUstvari.Name = "btnUstvari";
+            this.btnUstvari.Size = new System.Drawing.Size(75, 23);
+            this.btnUstvari.TabIndex = 3;
+            this.btnUstvari.Text = "Ustvari";
+            this.btnUstvari.UseVisualStyleBackColor = true;
+            this.btnUstvari.Click += new System.EventHandler(this.btnUstvari_Click);
+            // 
+            // tbxUstvari
+            // 
+            this.tbxUstvari.Location = new System.Drawing.Point(12, 12);
+            this.tbxUstvari.Name = "tbxUstvari";
+            this.tbxUstvari.Size = new System.Drawing.Size(100, 22);
+            this.tbxUstvari.TabIndex = 2;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(136, 40);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 23);
+            this.btnDodaj.TabIndex = 5;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // tbxDodaj
+            // 
+            this.tbxDodaj.Location = new System.Drawing.Point(12, 40);
+            this.tbxDodaj.Name = "tbxDodaj";
+            this.tbxDodaj.Size = new System.Drawing.Size(100, 22);
+            this.tbxDodaj.TabIndex = 4;
+            // 
+            // btnOdstrani
+            // 
+            this.btnOdstrani.Location = new System.Drawing.Point(136, 68);
+            this.btnOdstrani.Name = "btnOdstrani";
+            this.btnOdstrani.Size = new System.Drawing.Size(75, 23);
+            this.btnOdstrani.TabIndex = 7;
+            this.btnOdstrani.Text = "Odstrani";
+            this.btnOdstrani.UseVisualStyleBackColor = true;
+            this.btnOdstrani.Click += new System.EventHandler(this.btnOdstrani_Click);
+            // 
+            // tbxOdstrani
+            // 
+            this.tbxOdstrani.Location = new System.Drawing.Point(12, 68);
+            this.tbxOdstrani.Name = "tbxOdstrani";
+            this.tbxOdstrani.Size = new System.Drawing.Size(100, 22);
+            this.tbxOdstrani.TabIndex = 6;
+            // 
+            // btnIsci
+            // 
+            this.btnIsci.Location = new System.Drawing.Point(136, 96);
+            this.btnIsci.Name = "btnIsci";
+            this.btnIsci.Size = new System.Drawing.Size(75, 23);
+            this.btnIsci.TabIndex = 9;
+            this.btnIsci.Text = "Išči";
+            this.btnIsci.UseVisualStyleBackColor = true;
+            this.btnIsci.Click += new System.EventHandler(this.btnIsci_Click);
+            // 
+            // tbxIsci
+            // 
+            this.tbxIsci.Location = new System.Drawing.Point(12, 96);
+            this.tbxIsci.Name = "tbxIsci";
+            this.tbxIsci.Size = new System.Drawing.Size(100, 22);
+            this.tbxIsci.TabIndex = 8;
+            // 
+            // btnPonastavi
+            // 
+            this.btnPonastavi.Location = new System.Drawing.Point(93, 415);
+            this.btnPonastavi.Name = "btnPonastavi";
+            this.btnPonastavi.Size = new System.Drawing.Size(75, 23);
+            this.btnPonastavi.TabIndex = 10;
+            this.btnPonastavi.Text = "Ponastavi";
+            this.btnPonastavi.UseVisualStyleBackColor = true;
+            // 
+            // pnlPrikaz
+            // 
+            this.pnlPrikaz.Location = new System.Drawing.Point(238, 12);
+            this.pnlPrikaz.Name = "pnlPrikaz";
+            this.pnlPrikaz.Size = new System.Drawing.Size(550, 426);
+            this.pnlPrikaz.TabIndex = 11;
+            this.pnlPrikaz.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlPrikaz_Paint);
+            // 
+            // btnNaprej
+            // 
+            this.btnNaprej.Location = new System.Drawing.Point(135, 386);
+            this.btnNaprej.Name = "btnNaprej";
+            this.btnNaprej.Size = new System.Drawing.Size(75, 23);
+            this.btnNaprej.TabIndex = 12;
+            this.btnNaprej.Text = "Naprej";
+            this.btnNaprej.UseVisualStyleBackColor = true;
+            // 
+            // btnNazaj
+            // 
+            this.btnNazaj.Location = new System.Drawing.Point(54, 386);
+            this.btnNazaj.Name = "btnNazaj";
+            this.btnNazaj.Size = new System.Drawing.Size(75, 23);
+            this.btnNazaj.TabIndex = 13;
+            this.btnNazaj.Text = "Nazaj";
+            this.btnNazaj.UseVisualStyleBackColor = true;
+            // 
+            // lblHitrost
+            // 
+            this.lblHitrost.AutoSize = true;
+            this.lblHitrost.Location = new System.Drawing.Point(12, 162);
+            this.lblHitrost.Name = "lblHitrost";
+            this.lblHitrost.Size = new System.Drawing.Size(80, 16);
+            this.lblHitrost.TabIndex = 14;
+            this.lblHitrost.Text = "Izberi hitrost:";
+            // 
+            // rdbPol
+            // 
+            this.rdbPol.AutoSize = true;
+            this.rdbPol.Location = new System.Drawing.Point(99, 157);
+            this.rdbPol.Name = "rdbPol";
+            this.rdbPol.Size = new System.Drawing.Size(51, 20);
+            this.rdbPol.TabIndex = 15;
+            this.rdbPol.TabStop = true;
+            this.rdbPol.Text = "0.5x";
+            this.rdbPol.UseVisualStyleBackColor = true;
+            // 
+            // rdbEna
+            // 
+            this.rdbEna.AutoSize = true;
+            this.rdbEna.Location = new System.Drawing.Point(99, 184);
+            this.rdbEna.Name = "rdbEna";
+            this.rdbEna.Size = new System.Drawing.Size(41, 20);
+            this.rdbEna.TabIndex = 16;
+            this.rdbEna.TabStop = true;
+            this.rdbEna.Text = "1x";
+            this.rdbEna.UseVisualStyleBackColor = true;
+            // 
+            // rdbRocno
+            // 
+            this.rdbRocno.AutoSize = true;
+            this.rdbRocno.Location = new System.Drawing.Point(99, 236);
+            this.rdbRocno.Name = "rdbRocno";
+            this.rdbRocno.Size = new System.Drawing.Size(132, 20);
+            this.rdbRocno.TabIndex = 17;
+            this.rdbRocno.TabStop = true;
+            this.rdbRocno.Text = "ročno premikanje";
+            this.rdbRocno.UseVisualStyleBackColor = true;
+            // 
+            // rdbEnaInPol
+            // 
+            this.rdbEnaInPol.AutoSize = true;
+            this.rdbEnaInPol.Location = new System.Drawing.Point(99, 210);
+            this.rdbEnaInPol.Name = "rdbEnaInPol";
+            this.rdbEnaInPol.Size = new System.Drawing.Size(51, 20);
+            this.rdbEnaInPol.TabIndex = 18;
+            this.rdbEnaInPol.TabStop = true;
+            this.rdbEnaInPol.Text = "1.5x";
+            this.rdbEnaInPol.UseVisualStyleBackColor = true;
+            // 
+            // lblRazlaga
+            // 
+            this.lblRazlaga.AutoSize = true;
+            this.lblRazlaga.Location = new System.Drawing.Point(9, 308);
+            this.lblRazlaga.Name = "lblRazlaga";
+            this.lblRazlaga.Size = new System.Drawing.Size(0, 16);
+            this.lblRazlaga.TabIndex = 20;
+            // 
+            // OknoVizualizacijaIDD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRazlaga);
+            this.Controls.Add(this.rdbEnaInPol);
+            this.Controls.Add(this.rdbRocno);
+            this.Controls.Add(this.rdbEna);
+            this.Controls.Add(this.rdbPol);
+            this.Controls.Add(this.lblHitrost);
+            this.Controls.Add(this.btnNazaj);
+            this.Controls.Add(this.btnNaprej);
+            this.Controls.Add(this.pnlPrikaz);
+            this.Controls.Add(this.btnPonastavi);
+            this.Controls.Add(this.btnIsci);
+            this.Controls.Add(this.tbxIsci);
+            this.Controls.Add(this.btnOdstrani);
+            this.Controls.Add(this.tbxOdstrani);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.tbxDodaj);
+            this.Controls.Add(this.btnUstvari);
+            this.Controls.Add(this.tbxUstvari);
+            this.Name = "OknoVizualizacijaIDD";
+            this.Text = "Vizualizacija IDD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUstvari;
+        private System.Windows.Forms.TextBox tbxUstvari;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.TextBox tbxDodaj;
+        private System.Windows.Forms.Button btnOdstrani;
+        private System.Windows.Forms.TextBox tbxOdstrani;
+        private System.Windows.Forms.Button btnIsci;
+        private System.Windows.Forms.TextBox tbxIsci;
+        private System.Windows.Forms.Button btnPonastavi;
+        private System.Windows.Forms.Panel pnlPrikaz;
+        private System.Windows.Forms.Button btnNaprej;
+        private System.Windows.Forms.Button btnNazaj;
+        private System.Windows.Forms.Label lblHitrost;
+        private System.Windows.Forms.RadioButton rdbPol;
+        private System.Windows.Forms.RadioButton rdbEna;
+        private System.Windows.Forms.RadioButton rdbRocno;
+        private System.Windows.Forms.RadioButton rdbEnaInPol;
+        private System.Windows.Forms.Label lblRazlaga;
+    }
+}
+
---
+++ VizualizacijaIDD/VizualizacijaIDD/OknoVizualizacijaIDD.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VizualizacijaIDD
+{
+    public partial class OknoVizualizacijaIDD : Form
+    {
+        IDD drevo = new IDD();
+        int trenutniKorak = 0; // indeks trenutne vrstice v seznamu Razlaga // SE ŠE NE POVEČUJE
+        // Timer casovnik = new Timer();
+
+        public OknoVizualizacijaIDD()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUstvari_Click(object sender, EventArgs e)
+        {
+            // MANJKA
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbxDodaj.Text, out int v))
+            {
+                drevo.Vstavi(v);
+                trenutniKorak = 0;
+                PrikaziKorak(); // prikažemo prvi korak
+            }
+        }
+
+        private void btnOdstrani_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbxOdstrani.Text, out int v))
+            {
+                drevo.Odstrani(v);
+                trenutniKorak = 0;
+                PrikaziKorak();
+            }
+        }
+
+        private void btnIsci_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbxIsci.Text, out int v))
+            {
+                drevo.Isci(v);
+                trenutniKorak = 0;
+                PrikaziKorak();
+            }
+        }
+
+        private void PrikaziKorak()
+        {
+            if (drevo.Razlaga.Count == 0)
+                return;
+
+            if (trenutniKorak < drevo.Razlaga.Count)
+            {
+                lblRazlaga.Text = drevo.Razlaga[trenutniKorak];
+                pnlPrikaz.Invalidate();
+            }
+        }
+
+        private void pnlPrikaz_Paint(object sender, PaintEventArgs e)
+        {
+            if (drevo.Koren != null)
+            {
+                NarisiDrevo(e.Graphics, drevo.Koren, pnlPrikaz.Width / 2, 30, pnlPrikaz.Width / 4);
+            }
+        }
+
+        public void NarisiDrevo(Graphics g, Vozlisce v, int x, int y, int dx)
+        {
+            if (v == null) 
+                return;
+
+            // Povezave
+            if (v.Levo != null)
+                g.DrawLine(Pens.Black, x, y, x - dx, y + 50);
+            if (v.Desno != null)
+                g.DrawLine(Pens.Black, x, y, x + dx, y + 50);
+
+            // Vozlišče
+            g.FillEllipse(Brushes.LightBlue, x - 15, y - 15, 30, 30);
+            g.DrawEllipse(Pens.Black, x - 15, y - 15, 30, 30);
+            g.DrawString(v.Vrednost.ToString(), new Font("Arial", 10), Brushes.Black, x - 10, y - 8);
+            NarisiDrevo(g, v.Levo, x - dx, y + 50, dx / 2);
+            NarisiDrevo(g, v.Desno, x + dx, y + 50, dx / 2);
+        }
+
+    }
+
+
+    public class Vozlisce
+    {
+        public int Vrednost;
+        public Vozlisce Levo;
+        public Vozlisce Desno;
+        public int X;
+        public int Y;
+    }
+
+    public class IDD
+    {
+        public Vozlisce Koren;
+        public List<string> Razlaga = new List<string>(); // vsebuje nize za razlago korakov
+
+        /// <summary>
+        /// Glavna funkcija za vstavljanje vrednosti v drevo, ki kliče pomožno funkcijo.
+        /// </summary>
+        public void Vstavi(int vrednost)
+        {
+            Razlaga.Clear();
+            Koren = VstaviPomozna(Koren, vrednost);
+        }
+
+        /// <summary>
+        /// Vstavi vrednost na ustrezno mesto v drevo s pomočjo rekurzije.
+        /// </summary>
+        private Vozlisce VstaviPomozna(Vozlisce trenutno, int vrednost)
+        {
+            int trenutnaVrednost = (trenutno != null) ? trenutno.Vrednost : -1; // če trenutno ni null, vzamemo trenutno.Vrednost, sicer -1
+            if (trenutno == null)
+            {
+                Razlaga.Add($"Dodajam vozlišče z vrednostjo {vrednost}.");
+                return new Vozlisce { Vrednost = vrednost };
+            }
+
+            if (vrednost == trenutno.Vrednost)
+            {
+                Razlaga.Add("Vozlišče s to vrednostjo že obstaja!");
+                return trenutno;
+            }
+
+            if (vrednost < trenutno.Vrednost)
+                trenutno.Levo = VstaviPomozna(trenutno.Levo, vrednost);
+            else
+                trenutno.Desno = VstaviPomozna(trenutno.Desno, vrednost);
+
+            return trenutno;
+        }
+
+        /// <summary>
+        /// Glavna funkcija za iskanje vrednosti v drevesu, ki kliče pomožno funkcijo.
+        /// </summary>
+        public bool Isci(int vrednost)
+        {
+            Razlaga.Clear();
+            return IsciPomozna(Koren, vrednost);
+        }
+
+        /// <summary>
+        /// Išče vrednost v drevesu s pomočjo rekurzije.
+        /// </summary>
+        private bool IsciPomozna(Vozlisce trenutno, int vrednost)
+        {
+            if (trenutno == null)
+            {
+                Razlaga.Add($"Vozlišče z vrednostjo {vrednost} ni bilo najdeno.");
+                return false;
+            }
+
+            Razlaga.Add($"Iščem vozlišče z vrednostjo {trenutno.Vrednost}");
+
+            if (vrednost == trenutno.Vrednost)
+            {
+                Razlaga.Add($"Vozlišče z vrednostjo {vrednost} je bilo najdeno.");
+                return true;
+            }
+
+            if (vrednost < trenutno.Vrednost)
+            {
+                Razlaga.Add($"Ker je {vrednost} < {trenutno.Vrednost}, gremo levo.");
+                return IsciPomozna(trenutno.Levo, vrednost);
+            }
+
+            else
+            {
+                Razlaga.Add($"Ker je {vrednost} > {trenutno.Vrednost}, gremo desno.");
+                return IsciPomozna(trenutno.Desno, vrednost);
+            }
+        }
+
+        /// <summary>
+        /// Glavna funkcija za brisanje vrednosti iz drevesa, ki kliče pomožno funkcijo.
+        /// </summary>
+        public void Odstrani(int vrednost)
+        {
+            Razlaga.Clear();
+            Koren = OdstraniPomozna(Koren, vrednost);
+        }
+
+        /// <summary>
+        /// Odstrani vrednost iz drevesa s pomočjo rekurzije.
+        /// </summary>
+        private Vozlisce OdstraniPomozna(Vozlisce trenutno, int vrednost)
+        {
+            if (trenutno == null)
+            {
+                Razlaga.Add($"Vozlišča z vrednostjo {vrednost} ne morem odstraniti, ker ne obstaja!");
+                return null;
+            }
+
+            Razlaga.Add($"Brišem vozlišče z vrednostjo {trenutno.Vrednost}.");
+
+            if (vrednost < trenutno.Vrednost)
+            {
+                Razlaga.Add($"Ker je {vrednost} < {trenutno.Vrednost}, gremo levo.");
+                trenutno.Levo = OdstraniPomozna(trenutno.Levo, vrednost);
+            }
+
+            else if (vrednost > trenutno.Vrednost)
+            {
+                Razlaga.Add($"Ker je {vrednost} > {trenutno.Vrednost}, gremo desno.");
+                trenutno.Desno = OdstraniPomozna(trenutno.Desno, vrednost);
+            }
+
+            else 
+            {
+                Razlaga.Add($"Vozlišče z vrednostjo {vrednost} je najdeno. Lahko ga izbrišemo.");
+
+                // vozlišče brez sinov
+                if (trenutno.Levo == null && trenutno.Desno == null)
+                {
+                    Razlaga.Add("Vozlišče nima sinov, zato ne rabimo nič popravljati.");
+                    return null;
+                }
+
+                // vozlišče z enim sinom
+                if (trenutno.Desno == null) // levi sin
+                {
+                    Razlaga.Add("Vozlišče ima samo levega sina. Premaknemo ga navzgor.");
+                    return trenutno.Levo;
+                }
+
+                if (trenutno.Levo == null) // desni sin
+                {
+                    Razlaga.Add("Vozlišče ima samo desnega sina. Premaknemo ga navzgor.");
+                    return trenutno.Desno;
+                }
+
+                // vozlišče z dvema sinovoma
+                Vozlisce naslednik = NajmanjsiDesno(trenutno.Desno);
+                Razlaga.Add($"Vozlišče ima dva sinova. Zamenjamo ga z naslednikom ({naslednik.Vrednost}).");
+                trenutno.Vrednost = naslednik.Vrednost;
+                trenutno.Desno = OdstraniPomozna(trenutno.Desno, naslednik.Vrednost); // zbrišemo naslednika iz desnega poddrevesa
+
+            }
+            return trenutno;
+        }
+
+        /// <summary>
+        /// Poišče najmanjšo vrednost v desnem poddrevesu.
+        /// </summary>
+        private Vozlisce NajmanjsiDesno(Vozlisce vozlisce)
+        {
+            while (vozlisce.Levo != null) // gremo levo, kjer so manjši elementi
+            {
+                vozlisce = vozlisce.Levo;
+            }
+            return vozlisce;
+        }
+
+    }
+}
