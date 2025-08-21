064691e4f0c7a67238e621ac78edf9af86489a4e
tejablazic <tejablazic12@gmail.com>
2025-08-21 21:48:09 +0200

    popravljeno začetno okno

--- VizualizacijaIDD/VizualizacijaIDD/ZacetnoOkno.Designer.cs
+++ VizualizacijaIDD/VizualizacijaIDD/ZacetnoOkno.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZacetnoOkno));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(0, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(640, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dobrodošli v programu Vizualizacija iskalnega dvojiškega drevesa (IDD)!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(2, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(636, 183);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(12, 270);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(616, 78);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ZacetnoOkno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ZacetnoOkno";
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ZacetnoOkno";
+            this.Text = "Začetno okno";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
     }
 }
--- VizualizacijaIDD/VizualizacijaIDD/ZacetnoOkno.cs
+++ VizualizacijaIDD/VizualizacijaIDD/ZacetnoOkno.cs
@@ -12,71 +12,23 @@ namespace VizualizacijaIDD
 {
     public partial class ZacetnoOkno: Form
     {
-        private Label lblNaslov;
-        private TextBox tbxNavodila;
-        private Button btnStart;
 
         public ZacetnoOkno()
         {
-            Text = "Vizualizacija IDD – Navodila";
-            StartPosition = FormStartPosition.CenterScreen;
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            ClientSize = new Size(640, 360);
-
-            lblNaslov = new Label
-            {
-                Text = "Dobrodošli v vizualizaciji iskalnega dvojiškega drevesa (IDD)",
-                AutoSize = false,
-                Font = new Font("Segoe UI", 12, FontStyle.Bold),
-                Dock = DockStyle.Top,
-                Height = 48,
-                TextAlign = ContentAlignment.MiddleCenter
-            };
-
-            tbxNavodila = new TextBox
-            {
-                Multiline = true,
-                ReadOnly = true,
-                BorderStyle = BorderStyle.FixedSingle,
-                ScrollBars = ScrollBars.Vertical,
-                Dock = DockStyle.Fill,
-                Font = new Font("Segoe UI", 10),
-                Text =
-@"Navodila:
-• V polje 'Ustvari' vnesi seznam števil, ločenih z vejicami (npr. 50,25,75,12,...), nato klikni USTVARI.
-• Z gumbi DODAJ, ODSTRANI in IŠČI lahko izvajaš operacije nad drevesom.
-• Izbrano (dodano/iskano/brisano) vozlišče se obarva.
-• Drevo se prilagaja velikosti okna; povečevalno okno mu pusti več prostora na desni.
-
-Klikni START za začetek."
-            };
-
-            btnStart = new Button
-            {
-                Text = "START",
-                Height = 36,
-                Dock = DockStyle.Bottom,
-                Font = new Font("Segoe UI", 11, FontStyle.Bold)
-            };
-            btnStart.Click += BtnStart_Click;
-
-            Controls.Add(tbxNavodila);
-            Controls.Add(btnStart);
-            Controls.Add(lblNaslov);
+            InitializeComponent();
         }
 
-        private void BtnStart_Click(object sender, EventArgs e)
+        private void btnStart_Click(object sender, EventArgs e)
         {
-            // Odpri glavno okno, skrij začetno; ko glavno zapreš, zapri še začetno -> app se zaključi
-            this.Hide();
-            var glavno = new OknoVizualizacijaIDD
-            {
-                StartPosition = FormStartPosition.CenterScreen
-            };
-            glavno.FormClosed += (s, args) => this.Close();
+            var glavno = new OknoVizualizacijaIDD();
+            glavno.FormClosed += Glavno_FormClosed;
             glavno.Show();
+            Hide(); // skrijemo začetno okno
+        }
+
+        private void Glavno_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close(); // ko zapremo glavno okno, zapremo tudi začetno - program se konča
         }
     }
 }
