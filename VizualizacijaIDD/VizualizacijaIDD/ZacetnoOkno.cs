ecdecb9dd7e2fa7e307195f428fefd15ea5a6b10
tejablazic <tejablazic12@gmail.com>
2025-08-29 11:30:10 +0200

    Začetno okno - popravek komentarjev

--- VizualizacijaIDD/VizualizacijaIDD/ZacetnoOkno.cs
+++ VizualizacijaIDD/VizualizacijaIDD/ZacetnoOkno.cs
@@ -21,14 +21,14 @@ namespace VizualizacijaIDD
         private void btnStart_Click(object sender, EventArgs e)
         {
             var glavno = new OknoVizualizacijaIDD();
-            glavno.FormClosed += Glavno_FormClosed;
-            glavno.Show();
+            glavno.FormClosed += zapriGlavno; // ko se glavno okno zapre, se izvede metoda zapriOkno
+            glavno.Show(); // prikažemo glavno okno
             Hide(); // skrijemo začetno okno
         }
 
-        private void Glavno_FormClosed(object sender, FormClosedEventArgs e)
+        private void zapriGlavno(object sender, FormClosedEventArgs e)
         {
-            Close(); // ko zapremo glavno okno, zapremo tudi začetno - program se konča
+            Close(); // ko zapremo glavno okno, zapremo tudi začetno, program se konča
         }
     }
 }
